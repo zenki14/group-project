707f870828900c52938dfa78d0cced8e1dffa43a
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-29 15:12:18 -0500

    slideshow rough

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,3 @@
-var url = "https://api.github.com/users/twhitacre";
-var button = $('makeRequest');
-
-//get around api limits
-// $.ajaxSetup({
-// 	headers: {
-
-// 		'Authorization': 'token fbkejfbsdkjfbsakjdfbksjadfbdas'
-// 	}
-// });
-
-//Template
-
-var templateStr = $('#userTemplate').html();
-var templateFun = _.template(templateStr);
-
-var request = function () {
-	$.getJSON(url)
-	.done( function (data) {
-		$('.userInfo').html(templateFun(data));
-	})
-	.fail( function () {
-		console.log('Request Failed');
-	});
-};
-
-
-button.on('click', function () {
-	request();
-});
 
 
 
