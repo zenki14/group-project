7e77d65588ba676621e1993845880dba84e81da4
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-31 18:02:26 -0500

    scroll buttons added

--- app/scripts/jason.js
+++ app/scripts/jason.js
@@ -1,31 +1,40 @@
 
+var $slider;
+var $slideContainer;
+var $slides;
 
+var width = 1110;
+var animationSpeed = 1500;
+var pause = 3000;
+var currentSlide = 1;
+var interval;
+var timerLeft;
+var timerRight;
+
+function startSlider() {
+  $slideContainer
+  .on('mouseenter', pauseSlider)
+  .on('mouseleave', startSlider);
+
+  interval = setInterval(function() {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+      if (++currentSlide === $slides.length-1) {
+        currentSlide = 0;
+        $slideContainer.css('margin-left', 0);
+      }
+    });
+  }, pause);
+}
+function pauseSlider() {
+  clearInterval(interval);
+  clearInterval(timerLeft);
+  clearInterval(timerRight);
+}
 $(function(){
 
-  var width = 1110;
-  var animationSpeed = 1000;
-  var pause = 3000;
-  var currentSlide = 1;
-
-  var $slider = $('#slider');
-  var $slideContainer = $slider.find('.slides');
-  var $slides = $slideContainer.find('.slide')
-
-  var interval;
-
-  function startSlider() {
-    interval = setInterval(function() {
-      $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-        if (++currentSlide === $slides.length) {
-          currentSlide = 0;
-          $slideContainer.css('margin-left', 0);
-        }
-      });
-    }, pause);
-  }
-  function pauseSlider() {
-    clearInterval(interval);
-  }
+   $slider = $('#slider');
+   $slideContainer = $slider.find('.slides');
+   $slides = $slideContainer.find('.slide');
 
   $slideContainer
   .on('mouseenter', pauseSlider)
@@ -33,6 +42,29 @@ $(function(){
 
   startSlider();
 
-
-
 });
+
+function moveRight() {
+  timerRight = setInterval(function() {
+  $slideContainer.animate(
+    {'margin-left': '+='+width}, animationSpeed, function() {
+      if (++currentSlide === $slides.length-1) {
+        currentSlide = 0;
+        $slideContainer.css('margin-left', 0);
+      }
+    });},pause);
+    pauseSlider();
+    setTimeout(startSlider, 3000);
+
+  }
+
+  function moveLeft() {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+      if (++currentSlide === $slides.length-1) {
+        currentSlide = 0;
+        $slideContainer.css('margin-left', 0);
+      }
+    });
+    pauseSlider();
+    setTimeout(startSlider, 3000);
+  }
